5ddf95a1ce97e0d43d7860a1b0c827fb6bc3a09b
lori <lori.hsu@gmail.com>
2013-01-28 11:44:00 -0800

    create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faceballs::Application.config.secret_token = '6da44d2eacd48376d54e6bbdab551d620692e9370d42c3b501ac6f4e6d5323ca4151aadbdcbd8683622e9f36522574581bc962337ec8ab243dae36d5dfc44944'
